4152b0120ef1b074e42e04d1bd11ef08ce6b3d89
Peggy Condon & Ruben Rhodes <student@epicodus.com>
2014-07-23 14:38:52 -0700

    second spec pass

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,6 +1,22 @@
 def title_case(title)
-  keep_lower_words = ["a", "an", "the", "for", "the", "of", "to", "and", "his", "her"]
-  title[0].upcase + title[1..-1]
+  keep_lower_words = ["a", "an", "the", "for", "the", "of", "to", "and", "his", "her", "is", "at"]
+  split_words = title.split(" ")
+  title_result = []
+
+
+  split_words.each do |word|
+    if keep_lower_words.include?(word)
+      title_result.push(word)
+    else
+      new_word = word[0].upcase + word[1..-1]
+      title_result.push(new_word)
+    end
+  end
+
+  title_result.join(" ")
+
 end
 
-puts title_case("cat")
+puts title_case ("this is a sentence")
+
+
--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -5,4 +5,13 @@ describe('title_case') do
   it("should change a word to title case") do
     title_case("cat").should(eq("Cat"))
   end
+
+  it("should change a word to title case") do
+    title_case("cat at home").should(eq("Cat at Home"))
+  end
+
+  it("should ensure rouge capital letters are turned to lowercase") do
+    title_case("fred the dOG looked wEsT").should(eq("Fred the Dog Looked West"))
+  end
+
 end
